0fb9af0f2608c1e58c262468ba8c9b9f56fd1f11
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-01-25 23:00:54 +0200

    Commit

---
+++ Lab/EvenAndOddSubtraction_05.java
@@ -0,0 +1,21 @@
+package Arrays.Lab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EvenAndOddSubtraction_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = Arrays.stream(scanner.nextLine().split((" "))).mapToInt(e -> Integer.parseInt(e)).toArray();
+
+        int evenSum = 0;
+        int oddSum = 0;
+        for ( int num : arr) {
+            if (num % 2 == 0) evenSum += num;
+            else oddSum += num;
+        }
+        int diff = evenSum - oddSum;
+        System.out.println(diff);
+    }
+}
