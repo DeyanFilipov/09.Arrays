8861c75334565edaa048759535e1b7033c4e351a
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-01-28 15:54:20 +0200

    Commit

---
+++ MoreExercises/LadyBugs_06.java
@@ -0,0 +1,83 @@
+package Arrays.MoreExercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LadyBugs_06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] bugsArr = new int[Integer.parseInt(scanner.nextLine())];
+        Arrays.fill(bugsArr, 0);
+        int[] indexOfLadyBugs = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        for (int indexes : indexOfLadyBugs) {
+            if (indexes < 0 || indexes >= bugsArr.length) {
+                continue;
+            } else {
+                bugsArr[indexes] = 1;
+            }
+        }
+        String move = scanner.nextLine();
+
+        while (!"end".equals(move)) {
+            String[] indexesToMove = move.split("\\s+");
+            int initialPosition = Integer.parseInt(indexesToMove[0]);
+            String direction = indexesToMove[1];
+            int countMoves = Integer.parseInt(indexesToMove[2]);
+
+            int newPosition = 0;
+
+            if (countMoves < 0) {
+                if (direction.equals("left")) {
+                    direction = "right";
+                    countMoves = Math.abs(countMoves);
+                } else if (direction.equals("right")) {
+                    direction = "left";
+                    countMoves = Math.abs(countMoves);
+                }
+            }
+            if (initialPosition < 0 || initialPosition >= bugsArr.length || bugsArr[initialPosition] == 0) {
+                move = scanner.nextLine();
+                continue;
+            } else if (countMoves == 0 && initialPosition <= bugsArr.length - 1) {
+                if (bugsArr[initialPosition] == 1) {
+                    bugsArr[initialPosition] = 0;
+                }
+            }
+            switch (direction) {
+                case "right":
+                    newPosition = initialPosition + countMoves;
+                    bugsArr[initialPosition] = 0;
+                    if (newPosition > bugsArr.length - 1) {
+                        bugsArr[initialPosition] = 0;
+                        break;
+                    } else {
+                        for (int i = newPosition; i < bugsArr.length; i+= countMoves) {
+                            if (bugsArr[i] == 0) {
+                                bugsArr[i] = 1;
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                case "left":
+                    newPosition = initialPosition - countMoves;
+                    bugsArr[initialPosition] = 0;
+                    if (newPosition < 0) {
+                        bugsArr[initialPosition] = 0;
+                        break;
+                    } else {
+                        for (int i = newPosition; i >= 0 ;i-= countMoves) {
+                            if (bugsArr[i] == 0) {
+                                bugsArr[i] = 1;
+                                break;
+                            }
+                        }
+                    }
+                    break;
+            }
+            move = scanner.nextLine();
+        }
+        Arrays.stream(bugsArr).forEach(item -> System.out.print(item + " "));
+    }
+}
